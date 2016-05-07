c0877fdf58a7abb339e4cf57a5153b26a3da591f
Rails Udemy course <railsudemycourse@gmail.com>
2016-05-07 13:07:50 +0000

    update categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -51,12 +51,15 @@ class ArticlesController < ApplicationController
   end
   
   private
+  
   def set_article
     @article = Article.find(params[:id])
   end
-    def article_params
-      params.require(:article).permit(:title, :description)
-    end
+  
+  def article_params
+    params.require(:article).permit(:title, :description, category_ids: [])
+  end
+  
   def require_same_user
     if current_user != @article.user and !current_user.admin?
       flash[:danger] = "You can only edit or delete your own articles"
